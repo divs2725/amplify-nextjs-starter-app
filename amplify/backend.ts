b62bdce77e802532c0e249f2667ced976a46d056
divs2725 <111196535+divs2725@users.noreply.github.com>
2023-12-14 17:06:25 +0530

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,12 @@
+// amplify/backend.ts
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 
-defineBackend({
+const backend = defineBackend({
   auth,
-  data,
+  data
 });
 
 // create the bucket and its stack
@@ -13,7 +15,6 @@ const bucket = new s3.Bucket(bucketStack, 'Bucket', {
   blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
 });
 
-
 // allow any authenticated user to read and write to the bucket
 const authRole = backend.resources.auth.resources.authenticatedUserIamRole;
 bucket.grantReadWrite(authRole);
@@ -21,5 +22,3 @@ bucket.grantReadWrite(authRole);
 // allow any guest (unauthenticated) user to read from the bucket
 const unauthRole = backend.resources.auth.resources.unauthenticatedUserIamRole;
 bucket.grantRead(unauthRole);
-
-
